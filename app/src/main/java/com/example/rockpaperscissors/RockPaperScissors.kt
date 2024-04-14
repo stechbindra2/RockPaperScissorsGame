a165156b55d65e7f24b011446869cd8d92e21f33
stechbindra2 <shashikantbind@ece.du.ac.in>
2024-04-15 01:17:21 +0530

    Final Commit

---
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -0,0 +1,39 @@
+package com.example.rockpaperscissors
+
+fun main(){
+    var computerChoice = ""
+    var playerChoice = ""
+    println("Rock, Paper or Scissor? enter your choice!")
+    playerChoice = readln()
+
+    var randomNumber = (1..3).random()
+
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        2 -> {
+            computerChoice = "Scissors"
+        }
+    }
+    println(computerChoice)
+
+    var winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "computer"
+
+    }
+    if(winner == "Tie"){
+        println("It's a tie")
+    }
+    else{
+        println("$winner Won!")
+    }
+
+}
